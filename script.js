f9de6ab8c09e3fec56f6b950aacbe8416ee4cb00
andrea_merlos <merlosandrea13@gmail.com>
2024-10-08 11:12:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const myButton = document.getElementById("myButton");
+
+
+function printMessage() {
+    const input = document.getElementById("searchtext");
+    alert("You Searched Up: " + input.value);
+}
